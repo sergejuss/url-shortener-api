8589d0c9840f2c43a3e41a9dc324251b46677b6e
sergejuss <sergejus.stankevicius@gmail.com>
2016-10-18 17:53:14 +0300

    a

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use('/', express.static(__dirname + '/public'));
 
 app.get('/else', function(req, res){
@@ -10,6 +12,6 @@ app.get('*', function(req, res){
 	res.send('404 not found');
 });
 
-app.listen(5000, function(){
-	console.log('App listening on port 5000')
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
