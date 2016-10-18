e43d6c0039fe7f067716f8393e566649183c3585
sergejuss <sergejus.stankevicius@gmail.com>
2016-10-18 17:29:04 +0300

    added 404

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.use('/', express.static(__dirname + '/public'));
 app.get('/else', function(req, res){
   res.send('some website');
 });
+app.get('*', function(req, res){
+	res.send('404 not found');
+});
 
 app.listen(8080, function(){
 	console.log('App listening on port 8080')
