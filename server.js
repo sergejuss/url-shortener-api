561a552587d3bfe71578d441ff4a7cb19318f4bd
sergejuss <sergejus.stankevicius@gmail.com>
2016-10-19 19:22:28 +0300

    second draft

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var url = process.env.MONGODB_URI;
 var express = require('express');
 var app = express();
 
-var insertEntry = function(db, entry, callback){
+var insertDoc = function(db, entry, callback){
 	var collection = db.collection('urls');
 	collection.insert(entry, function(err, result){
 		if(err) console.log(err);
@@ -36,18 +36,15 @@ var deleteDocs = function(db, query, callback){
 	});
 };
 
-
-
 app.set('port', (process.env.PORT || 5000));
 
 app.use('/', express.static(__dirname + '/public'));
-
 
 app.get('/new/*', function(req, res){	
 	var regexp = /^https?:\/\/[a-z]+.+\.[a-z]{2,3}(?:\:[\d]+)?$/i;	
 	if (!regexp.test(req.originalUrl.slice(5))) {
 		var bad = 'bad request \n' + req.originalUrl.slice(5);
-		res.send(bad);
+		res.send({'error': 'Wrong url format, make sure you have a valid protocol and real site.'});
 	} else {		
 		mongo.connect(url, function(err, db){
 			if(err) {
@@ -64,29 +61,51 @@ app.get('/new/*', function(req, res){
 					});
 					if (origUrlArray.indexOf(req.originalUrl.slice(5)) !== -1) {
 						console.log('Such url allready exists');
-						res.send('Such url allready exists');
+						findDocs(db, {original_url: req.originalUrl.slice(5)}, {_id:0}, function(result){
+							res.send({'error': 'This url allready exists in the database.', 'original_url': result[0]['original_url'],
+								'short_url': ('https://sergejuss-url-shortener.herokuapp.com/'+result[0]['short_url'])});
+						});						
 					} else {
 						var sUrlValue = 1;
 						if (shortUrlArray.length>0) {
 							sUrlValue = shortUrlArray.sort(function(a, b){return a-b;})[shortUrlArray.length-1] + 1;							
 						}
 						var entry = {original_url: req.originalUrl.slice(5), short_url: sUrlValue};
-						insertEntry(db, entry, function(){							
-							res.send({'original_url': req.originalUrl.slice(5), 'short_url': ('http://localhost:5000/'+sUrlValue)});
+						insertDoc(db, entry, function(){							
+							res.send({'original_url': req.originalUrl.slice(5),
+								'short_url': ('https://sergejuss-url-shortener.herokuapp.com/'+sUrlValue)});
 						});						
 					}					
 					db.close();					
 				});
 			}
-		});
+		});		
+	}
+});
 
-		
-	}  	
-	
-});
+app.get(/[\d]+/, function(req, res){	
+	mongo.connect(url, function(err, db){
+		if(err) {
+			console.log(err);
+			res.send('Error connecting to the database');
+		} else {
+			console.log('db connected to', url);
+			findDocs(db, {short_url: +req.path.slice(1)}, {_id:0}, function(result){
+				if (result.length === 0) {
+					res.send({'error': 'This url is not on the database.'});
+				} else {					
+					res.redirect(result[0]['original_url']);					
+				}				
+				db.close();
+			});
+		}
+	});
+});	
+
 app.get('*', function(req, res){
-	res.send('404 not found');
+	res.send({'error': 'This url is not on the database.'});
 });
+
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));    
